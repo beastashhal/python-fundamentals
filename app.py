e368c6f24be0bfa7a7172a2bef1ffa6806ba8a0c
Muhammad Ashhal <muhammadashhal22@gmail.com>
2025-07-23 16:19:42 +0500

    Add files via upload
    A beginner-level Python script to practice print statements, user input, and basic data types.

---
+++ app.py
@@ -0,0 +1,14 @@
+#Basic
+print("Hello world")
+
+#Input Variables
+
+name = input('What is your name? ')
+color = input ('What is your favourite color? ')
+print(name + ' likes ' + color)
+
+#Data Types
+full_name = 'Ashhal' #String
+age = 19 #Integer
+merit_score = 91.15 #Flow
+ned_admission = True
